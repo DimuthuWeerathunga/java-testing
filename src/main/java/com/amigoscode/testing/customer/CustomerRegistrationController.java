2ff560c0769d3f629bca7b4a7fff6d5e4f83e242
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-01-29 16:12:51 +0530

    customer registration controller configured

--- src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
+++ src/main/java/com/amigoscode/testing/customer/CustomerRegistrationController.java
@@ -1,5 +1,7 @@
 package com.amigoscode.testing.customer;
 
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customer-registration")
 public class CustomerRegistrationController {
 
-    public void registerNewCustomer(CustomerRegistrationRequest request){
+    @PutMapping
+    public void registerNewCustomer(@RequestBody CustomerRegistrationRequest request){
 
     }
 
