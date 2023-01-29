b14ac41f3cda520eaba5d5628c1e18e523b00aff
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-01-29 16:43:57 +0530

    custmer repository added

---
+++ src/main/java/com/amigoscode/testing/customer/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.amigoscode.testing.customer;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+}
