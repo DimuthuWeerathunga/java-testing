bc820c83690cc9c9abcbc69a8a84283c822ab947
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-01-29 19:58:48 +0530

    first customer service test added

--- src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
@@ -1,10 +1,19 @@
 package com.amigoscode.testing.customer;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
 class CustomerRegistrationServiceTest {
@@ -12,11 +21,36 @@ class CustomerRegistrationServiceTest {
 //    @Mock
     private CustomerRepository customerRepository = mock(CustomerRepository.class);
 
+    @Captor
+    private ArgumentCaptor<Customer> customerArgumentCaptor;
+
     private CustomerRegistrationService underTest;
 
     @BeforeEach
     void setUp() {
 //        MockitoAnnotations.initMocks(this);
-        underTest = new CustomerRegistrationService();
+        underTest = new CustomerRegistrationService(customerRepository);
+    }
+
+    @Test
+    void itShouldSaveNewCustomer() {
+        // Given a phone number and a customer
+        String phoneNumber = "000099";
+        Customer customer = new Customer(UUID.randomUUID(), "Mariyam", phoneNumber);
+         
+        // a request
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(customer);
+
+        // no customer with phone number passed
+        given(customerRepository.selectCustomerByPhoneNumber(phoneNumber))
+                .willReturn(Optional.empty());
+
+        // When
+        underTest.registerNewCustomer(request);
+
+        // Then
+        then(customerRepository).should().save(customerArgumentCaptor.capture());
+        Customer customerArgumentCaptorValue = customerArgumentCaptor.getValue();
+        assertThat(customerArgumentCaptorValue).isEqualToComparingFieldByField(customer);
     }
 }
