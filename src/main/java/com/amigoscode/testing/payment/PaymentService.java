1a776bb213c23866f0264210d01caf99a33b3850
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-01-30 17:14:15 +0530

    payment service configuration

--- src/main/java/com/amigoscode/testing/payment/PaymentService.java
+++ src/main/java/com/amigoscode/testing/payment/PaymentService.java
@@ -4,21 +4,37 @@ import com.amigoscode.testing.customer.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class PaymentService {
 
+    private static final List<Currency> ACCEPTED_CURRENCIES = List.of(Currency.USD, Currency.GBP);
+
     private final CustomerRepository customerRepository;
+
     private final PaymentRepository paymentRepository;
 
+    private final CardPaymentCharger cardPaymentCharger;
+
     @Autowired
-    public PaymentService(CustomerRepository customerRepository, PaymentRepository paymentRepository) {
+    public PaymentService(CustomerRepository customerRepository, PaymentRepository paymentRepository, CardPaymentCharger cardPaymentCharger) {
         this.customerRepository = customerRepository;
         this.paymentRepository = paymentRepository;
+        this.cardPaymentCharger = cardPaymentCharger;
     }
 
-    void chargeCard(UUID customerId, PaymentRequest request){
+    void chargeCard(UUID customerId, PaymentRequest paymentRequest){
+        // 1. Does customer exist if not throw
+        boolean isCustomerFound = customerRepository.findById(customerId).isPresent();
+        if(!isCustomerFound){
+            throw new IllegalStateException(String.format("Customer with [%s] id not found", customerId));
+        }
+
+        // 2. Do we support the currency if not throw
+        ACCEPTED_CURRENCIES.stream()
+                .anyMatch(c -> c.equals(paymentRequest.))
 
     }
 }
