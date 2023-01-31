fb97c68390a2ea686981eb4edb0689a49b78d447
Dimuthu Weerathunga <dimu.wa.98@gmail.com>
2023-01-31 17:29:03 +0530

    payment integration test configuration

--- src/test/java/com/amigoscode/testing/payment/PaymentIntegrationTest.java
+++ src/test/java/com/amigoscode/testing/payment/PaymentIntegrationTest.java
@@ -2,10 +2,12 @@ package com.amigoscode.testing.payment;
 
 import com.amigoscode.testing.customer.Customer;
 import com.amigoscode.testing.customer.CustomerRegistrationRequest;
+import com.amigoscode.testing.customer.CustomerRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -13,16 +15,25 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.UUID;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+//@DataJpaTest
 public class PaymentIntegrationTest {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,15 +49,36 @@ public class PaymentIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(Objects.requireNonNull(objectToJson(customerRegistrationRequest))));
 
+//        customerRepository.save(customer);
+        Payment payment = new Payment(
+                1L,
+                customerId,
+                new BigDecimal("100.00"),
+                Currency.GBP,
+                "x0x0x0x0",
+                "Donations");
+        PaymentRequest paymentRequest = new PaymentRequest(payment);
+
         // When
+        ResultActions paymentResultActions = mockMvc.perform(
+                post("/api/v1/payment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(Objects.requireNonNull(objectToJson(paymentRequest)))
+        );
 
         // Then
         customerRegResultActions.andExpect(status().isOk());
+        paymentResultActions.andExpect(status().isOk());
+
+        assertThat(paymentRepository.findById(1L))
+                .isPresent()
+                .hasValueSatisfying(
+                        p -> assertThat(p).isEqualToComparingFieldByField(payment));
     }
 
-    private String objectToJson(Object customer) {
+    private String objectToJson(Object o) {
         try {
-            return new ObjectMapper().writeValueAsString(customer);
+            return new ObjectMapper().writeValueAsString(o);
         } catch (JsonProcessingException e) {
             fail("Failed to convert object to json");
             return null;
